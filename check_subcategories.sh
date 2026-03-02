11d0e7b524e0b05090c3f49ac1cf8e7c83e3c958
ivanov7marketing <idea174ru@gmail.com>
2026-03-02 17:33:57 +0500

    chore: remove debug scripts

--- check_subcategories.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /opt/chatbot
-docker compose exec -T postgres psql -U chatbot -d chatbot -c "SELECT subcategory, count(*) FROM work_types WHERE category='Демонтажные работы' GROUP BY subcategory ORDER BY subcategory;"
